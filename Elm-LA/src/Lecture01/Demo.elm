b87faed8931908694ef7b8092a3e03398ed4ecdf
Nathan Mull <nmmull@gmail.com>
2024-09-03 18:38:56 -0400

    demo

--- Elm-LA/src/Lecture01/Demo.elm
+++ Elm-LA/src/Lecture01/Demo.elm
@@ -6,15 +6,15 @@ import Lib.Space exposing (basic3D, Element(..))
 main =
     let
         p1 = Point3D 0 0 5
-        -- p2 = Point3D 0 -5 0
-        -- p3 = Point3D -5 0 0
+        p2 = Point3D 0 -5 0
+        p3 = Point3D -5 0 0
         pntSty = { color = Red, hasGuides = True }
-        pln = Plane3D 0 0 1 5
+        pln = Plane3D -1 -1 1 5
         plnSty = { color = Red, hasHatch = True }
     in
     basic3D
         [ Plane pln plnSty
         , Point p1 pntSty
-        -- , Point p2 pntSty
-        -- , Point p3 pntSty
+        , Point p2 pntSty
+        , Point p3 pntSty
         ]
